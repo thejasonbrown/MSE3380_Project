e8b349e564c0f5475f53befeff75c8596d6cd421
DESKTOP-1SICJ7B\Kaspar Shahzada <kshahza@uwo.ca>
2017-03-22 19:01:21 -0400

    Tooth Stress Calculator Cleaned Up and Tested *Needs two coefficients converted though

--- FOS.m
+++ FOS.m
@@ -1,118 +1,144 @@
 %% FOS Calculator
 % Function takes in two gear and the loading scenario to calculate FOS
 
-%% Inputs 
+%% Inputs
+%Gear Info
+material = struct('Name', 'Steel',...
+    'H', 200,...
+    'Grade', 1,...
+    'E', 210e3,...
+    'v', 0.3);
+gearSet = struct('Material', material,...
+    'DiametralPitch', 0.4,... %mm
+    'PitchAngle', 20,... %Degrees
+    'FaceWidth', 18,... %mm
+    'PinionTeeth', 20,...
+    'GearTeeth', 36,...
+    'Quality', 6);
+
+clear material;
+
+%load condition
+operatingConditions = 0; %**************
 power = 120; %watts
-Qv = 6; %Quality Factor assumed of 6
-
-
-hardness = 200; %brinell
-grade = 1;
-
-diametralPitch = 25.4/2.5; %teeth/Inch
-diametralPitchMetric = diametralPitch/25.4; %teeth/mm
-
-fwPinion = 18/25.4;
-tPinion = 20; %number of teeth
 nPinion = 100; %RPM
-bPinion = fwPinion*25.4; %face width in mm
-hardnessPinion = 0;
 
-tGear = 36; %numberOfTeeth
-
+%Service Requirements
 life = 10^8;
 reliability = 0.99;
 
-
 %% Processing
-St = GetMaxAllowableBendingStress(grade, hardness); %MPa
-Sc = GetMaxAllowableContactStress(grade, hardness); %MPa
 
+%Determine Auxilary Values
+dPinion = gearSet.PinionTeeth/gearSet.DiametralPitch; %diameter of pinion [mm]
+V = pi * (dPinion / 1000) * (nPinion / 60); % pitch line velocity [m/s]
+gearRatio = gearSet.GearTeeth/gearSet.PinionTeeth;
 
-pitchDiameterPinion = tPinion/diametralPitchMetric;
-V = pi * (pitchDiameterPinion / 1000) * (nPinion / 60); %m/s
-
+%Calculate Tooth Stress Coefficients
 wt = power/V; %Newtons
-
-%Assume Kb=Ko=Yo=Zr=1
-K0 = 1;
-
-
-%Dynamic Factor
-Kv = DynamicFactor(Qv, V);
-
-%Form Factors
-Yp = FormFactor(tPinion);
-Yg = FormFactor(tGear);
-
-Ks = 0.8433 * (bPinion * sqrt(Yp) / diametralPitchMetric)^0.0535;
-
-
-Kh = 1.27%LoadDistributionFactor(fwPinion, diametralPitch*tPinion);
-
+Kv = DynamicFactor(gearSet.Quality, V); %Dynamic Factor
+Ks = SizeFactor(gearSet); %Size Factor
+Kh = 1.27;%***********LoadDistributionFactor(fwPinion, diametralPitch*tPinion);
 Kb = 1; %Rim-Thickness Factor
+Yj = [0.33 0.038]; %****************Hard coded for now * Geometric Bending Strength
+Zi = PitResistanceExtSpur(gearSet.PitchAngle, gearRatio); %Geometric Pitting Resistance
+Ze = ElasticCoefficient(gearSet.Material); %Material-Material Gear Modulus
 
-YjP = 0.33; %****************Hard coded for now
-
-Y0 = 1;
-Zr = 1;
-%% Outputs
-bendingStressPinion = wt * K0 * Kv * Ks * (diametralPitchMetric/bPinion) * (Kh * Kb)/YjP
+%Calculate Tooth Stresses [MPa]
+bendingStressPinion = wt * K0 * Kv * Ks(1) * (gearSet.DiametralPitch/gearSet.FaceWidth) * (Kh * Kb)/Yj(1)
+bendingStressGear = wt * K0 * Kv * Ks(2) * (gearSet.DiametralPitch/gearSet.FaceWidth) * (Kh * Kb)/Yj(2)
+contactStressPinion = Ze *  sqrt(wt * K0 * Kv * Ks(1) * (Kh/((gearSet.PinionTeeth / gearSet.DiametralPitch) * gearSet.FaceWidth)) * (Zr/Zi))
+contactStressGear = (Ks(2)/Ks(1))^0.5 * contactStressPinion
 
 %% Auxilary Functions
 % Lookup Functions
 
 %% Max Bending Stress Calculator
-function [St] = GetMaxAllowableBendingStress (g, h)
-    if(g == 1) %Figure 14-2
-        St = 0.553 * h + 88.3; %MPa
-    elseif (g == 2)
-        St = 0.703 * h + 113; %MPa
-    end
+function [St] = GetMaxAllowableBendingStress (material)
+h  = material.H;
+g = material.Grade;
+if(g == 1) %Figure 14-2
+    St = 0.553 * h + 88.3; %MPa
+elseif (g == 2)
+    St = 0.703 * h + 113; %MPa
+end
 end
 
 %% Max Contact Stress Calculator
-function [Sc] = GetMaxAllowableContactStress (g, h)
-    if(g == 1) %Figure 14-2
-        Sc = 2.22 * h + 200; %MPa
-    elseif (g == 2)
-        Sc = 2.41 * h + 237; %MPa
-    end
+function [Sc] = GetMaxAllowableContactStress (material)
+h  = material.H;
+g = material.Grade;
+if(g == 1) %Figure 14-2
+    Sc = 2.22 * h + 200; %MPa
+elseif (g == 2)
+    Sc = 2.41 * h + 237; %MPa
+end
 end
 
 %% Dynamic Factor Calculator
 function [ Kv ] = DynamicFactor( Qv, vel )
-%Calculate Dynamic Factor   
-    b = 0.25 * (12 - Qv)^(2/3); %Eqn 14-28
-    a = 50 + 56 *(1 - b); %Eqn 14-28
+%Calculate Dynamic Factor
+b = 0.25 * (12 - Qv)^(2/3); %Eqn 14-28
+a = 50 + 56 *(1 - b); %Eqn 14-28
 
-    Kv = ((a + sqrt(200*vel))/a)^b; %Eqn 14-27
+Kv = ((a + sqrt(200*vel))/a)^b; %Eqn 14-27
 end
 
 %% Form Factor Lookup
 function [ Y ] = FormFactor(teeth)
-   if(teeth>400)
-       Y = 0.485;
-   else
-   load FormFactorTable.mat; %Table 14-2
-   Y = interp1(FormFactorTable(:,1)', FormFactorTable(:,2)', teeth);
-   end
-   clear FormFactorTable
+if(teeth>400)
+    Y = 0.485;
+else
+    load FormFactorTable.mat; %Table 14-2
+    Y = interp1(FormFactorTable(:,1)', FormFactorTable(:,2)', teeth);
+end
+clear FormFactorTable
 end
 
 %% Load Distribution Factor Calculator
 function [Kh] = LoadDistributionFactor(F, d)
+Fd = F/(10*d);
+if (Fd < 0.05)
+    Fd = 0.05;
+end
+
+%Find Cpf
 if (F <=1)
-    Cpf=(F/(10*d)-0.025);
+    Cpf=(Fd-0.025);
 elseif (F <= 17)
-    Cpf=(d/(10*d)-0.0375+0.0125*F);
+    Cpf=(Fd-0.0375+0.0125*F);
 elseif (F<=40)
-    Cpf=(F/(10*d)-0.1109+0.0207*F-0.000228*F^2);
-end 
+    Cpf=(Fd-0.1109+0.0207*F-0.000228*F^2);
+end
 Cmc = 1; %for uncrowned teeth
 Cpm = 1; %straddle-mounted
 Cma = 0.127 + 0.0158 * F - 0.930 * 10^-4 * F^2; %commercial enclosed unit
 Ce = 1; % gearing is not adjusted at assembly, nor compatibility is improved by lapping
-
+%**************Needs ABC lookup table for Cma
 Kh = 1 + Cmc*(Cpf*Cpm+Cma*Ce);
+end
+
+%% Pit Resistance Calculator
+function [Zi] = PitResistanceExtSpur(pitchAngle, gearRatio)
+Zi = ((cosd(pitchAngle)*sind(pitchAngle)) / 2) * (gearRatio/(gearRatio + 1)); %External Spur (mN = 1)
+end
+
+%% Elastic Coefficient (assume repeated material)
+function [Ze] = ElasticCoefficient(material)
+E = material.E;
+v = material.v;
+
+Ze = sqrt(E / (2 * pi * (1 - v^2)));
+end
+
+%% Size Factor Calculator
+function [Ks] = SizeFactor(gearSet)
+%Form Factors
+Yp = FormFactor(gearSet.PinionTeeth);
+Yg = FormFactor(gearSet.GearTeeth);
+
+KsP = 0.8433 * (gearSet.FaceWidth * sqrt(Yp) / gearSet.DiametralPitch)^0.0535;
+KsG = 0.8433 * (gearSet.FaceWidth * sqrt(Yg) / gearSet.DiametralPitch)^0.0535;
+
+Ks = [KsP KsG];
 end
