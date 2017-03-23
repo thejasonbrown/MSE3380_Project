570d2b0785fc8cb5c15f0e8990d104b74d528742
thejasonbrown <jas.brown@outlook.com>
2017-03-23 19:34:07 -0400

    Cleaned Up output_power - FOS and Efficiency
    Left in FOS, took out efficiency

--- output_power.m
+++ output_power.m
@@ -1,21 +1,14 @@
-function [RequiredOutputPower,RequiredOuputTorque,RequiredOuputVelocity] = output_power(BlankLoad,BeltMass,Jroller,nRollers,RollerRadius,Speed,Friction)
+function [FOSOutputPower,FOSOutputTorque,RequiredOutputVelocity] = output_power(BlankLoad,BeltMass,RollerRadius,Speed,Friction)
 %This function calculates the required output power, torque, and speed
+%(including factor of safety)
 
-Tension = (BlankLoad+BeltMass)*9.81*Friction; % N Tension in a horizontal belt is determined by mass of belt, load, and friction
-%including efficiency
-Efficiency = 0.9;
-RunningTorque = RollerRadius * Tension; % N.m (Torque required to run the system)
-StartingTorque = RunningTorque*1.2/Efficiency; %N.m 20% FOS & 90% efficiency
+Force = (BlankLoad+BeltMass)*9.81*Friction; % N Tension in a horizontal belt is determined by mass of belt, load, and friction
+TheoreticalOutputTorque = RollerRadius * Force; % N.m (Torque required to run the system)
+FOSOutputTorque = TheoreticalOutputTorque*1.2; %N.m 20% FOS
 
-%Not used anywhere
-RequiredOuputVelocity = Speed*60/(2*pi()*RollerRadius); %Gives the angular velocity in RPM required for linear speed of 0.1m/s
-RequiredOuputTorque = StartingTorque;
-RequiredOutputPower = (RequiredOuputTorque*RequiredOuputVelocity*pi()*2/60)/1000; %Torque multiplied by W [rad/s] gives power in kW
+RequiredOutputVelocity = Speed*60/(2*pi*RollerRadius); %Gives the angular velocity in RPM required for linear speed of 0.1m/s
 
-
-%Equations sourced from:
-%http://www.brighthubengineering.com/manufacturing-technology/83551-onsite-calculations-for-conveyor-belt-systems/
-%http://chain-guide.com/basics/2-3-1-coefficient-of-friction.html
+FOSOutputPower = (FOSOutputTorque*RequiredOutputVelocity*pi*2/60)/1000 %Torque multiplied by W [rad/s] gives power in kW
 
 end
 
