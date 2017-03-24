75e312835f5c60a89eb1982cb18b57982c67ce4b
mikeallan <mike@mikeallan.ca>
2017-03-24 00:37:08 -0400

    Deleted unnecessary functions

--- GearNumCalc.m
+++
@@ -1,3 +0,0 @@
-function [MaxGearTeeth] = GearNumCalc(k,pinionTeeth,PressureAngle)
-MaxGearTeeth= floor((pinionTeeth^2*(sind(PressureAngle))^2-4*k^2)/(4*k-2*pinionTeeth*(sind(PressureAngle))^2));
-end
