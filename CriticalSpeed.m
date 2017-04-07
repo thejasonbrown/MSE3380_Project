23f9f8d1ac87dcfcacecb8c4402ba3302043fbdb
Jason Brown <jas.brown@outlook.com>
2017-04-07 16:25:45 -0400

    More for critical speed

--- CriticalSpeed.m
+++ CriticalSpeed.m
@@ -1,5 +1,8 @@
-function [omega] = CriticalSpeed(shaft, forces)
+function [omega] = CriticalSpeed(diameter, forces, deflection)
 % This function calculates the critical speeds of the loaded shafts
-% Note: shaft struct has [], and forces has []
+% Note: shaft struct has (position [micrometer], diameter [millimeter]), forces
+% has (position [micrometer], force [N]), and deflection ()
+
+w_shaft =       % shaft's intrinsic critical speed
 
 end
